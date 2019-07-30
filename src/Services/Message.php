77ecd90838d2592e7ff4e56a6db5d04af85a9290
nCuXaP <g.prusiyski@webrika.bg>
2019-07-30 23:30:33 +0000

    hot fix

--- src/Services/Message.php
+++ src/Services/Message.php
@@ -11,120 +11,120 @@ use Google_Service_Gmail;
 class Message
 {
 
-    use SendsParameters,
-        Filterable;
+	use SendsParameters,
+		Filterable;
 
-    public $service;
+	public $service;
 
-    public $preload = false;
+	public $preload = false;
 
-    public $pageToken;
+	public $pageToken;
 
-    /**
-     * Optional parameter for getting single and multiple emails
-     *
-     * @var array
-     */
-    protected $params = [];
+	/**
+	 * Optional parameter for getting single and multiple emails
+	 *
+	 * @var array
+	 */
+	protected $params = [];
 
-    /**
-     * Message constructor.
-     *
-     * @param  LaravelGmailClass  $client
-     */
-    public function __construct(LaravelGmailClass $client)
-    {
-        $this->service = new Google_Service_Gmail($client);
-    }
+	/**
+	 * Message constructor.
+	 *
+	 * @param  LaravelGmailClass  $client
+	 */
+	public function __construct(LaravelGmailClass $client)
+	{
+		$this->service = new Google_Service_Gmail($client);
+	}
 
-    /**
-     * Returns next page if available of messages or an empty collection
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function next()
-    {
-        if ($this->pageToken) {
-            return $this->all($this->pageToken);
-        } else {
-            return collect([]);
+	/**
+	 * Returns next page if available of messages or an empty collection
+	 *
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function next()
+	{
+		if ($this->pageToken) {
+			return $this->all($this->pageToken);
+		} else {
+			return collect([]);
+		}
+	}
+
+	/**
+	 * Returns a collection of Mail instances
+	 *
+	 * @param  null|string  $pageToken
+	 *
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function all($pageToken = null)
+	{
+		if (!is_null($pageToken)) {
+			$this->add($pageToken, 'pageToken');
+		}
+
+		$messages = [];
+		$response = $this->service->users_messages->listUsersMessages('me', $this->params);
+
+		$this->pageToken = $response->getNextPageToken();
+
+		$allMessages = $response->getMessages();
+
+		foreach ($allMessages as $message) {
+			$messages[] = new Mail($message, $this->preload);
         }
-    }
 
-    /**
-     * Returns a collection of Mail instances
-     *
-     * @param  null|string  $pageToken
-     *
-     * @return \Illuminate\Support\Collection
-     */
-    public function all($pageToken = null)
-    {
-        if (!is_null($pageToken)) {
-            $this->add($pageToken, 'pageToken');
-        }
+		return collect($messages);
+	}
 
-        $messages = [];
-        $response = $this->service->users_messages->listUsersMessages('me', $this->params);
+	/**
+	 * Returns boolean if the page token variable is null or not
+	 *
+	 * @return bool
+	 */
+	public function hasNextPage()
+	{
+		return !!$this->pageToken;
+	}
 
-        $this->pageToken = $response->getNextPageToken();
+	/**
+	 * Limit the messages coming from the query
+	 *
+	 * @param  int  $number
+	 *
+	 * @return Message
+	 */
+	public function take($number)
+	{
+		$this->params['maxResults'] = abs((int) $number);
 
-        $allMessages = $response->getMessages();
+		return $this;
+	}
 
-        foreach ($allMessages as $message) {
-            $messages[] = new Mail($message, $this->preload);
-        }
+	/**
+	 * @param $id
+	 *
+	 * @return Mail
+	 */
+	public function get($id)
+	{
+		$message = $this->service->users_messages->get('me', $id);
 
-        return collect($messages);
-    }
+		return new Mail($message);
+	}
 
-    /**
-     * Returns boolean if the page token variable is null or not
-     *
-     * @return bool
-     */
-    public function hasNextPage()
-    {
-        return !!$this->pageToken;
-    }
+	/**
+	 * Preload the information on each Mail objects.
+	 * If is not preload you will have to call the load method from the Mail class
+	 * @return $this
+	 * @see Mail::load()
+	 *
+	 */
+	public function preload()
+	{
+		$this->preload = true;
 
-    /**
-     * Limit the messages coming from the query
-     *
-     * @param  int  $number
-     *
-     * @return Message
-     */
-    public function take($number)
-    {
-        $this->params['maxResults'] = abs((int) $number);
-
-        return $this;
-    }
-
-    /**
-     * @param $id
-     *
-     * @return Mail
-     */
-    public function get($id)
-    {
-        $message = $this->service->users_messages->get('me', $id);
-
-        return new Mail($message);
-    }
-
-    /**
-     * Preload the information on each Mail objects.
-     * If is not preload you will have to call the load method from the Mail class
-     * @return $this
-     * @see Mail::load()
-     *
-     */
-    public function preload()
-    {
-        $this->preload = true;
-
-        return $this;
-    }
+		return $this;
+	}
 }
