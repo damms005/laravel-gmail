05f3baa1bb07613ae09c8d5dac4c3311cbe64f90
Dean Hopkins <deanhopkins@gmail.com>
2020-06-29 14:01:23 +0100

    Fix paging

--- src/Services/Message.php
+++ src/Services/Message.php
@@ -6,125 +6,139 @@ use Amchara\LaravelGmail\LaravelGmailClass;
 use Amchara\LaravelGmail\Services\Message\Mail;
 use Amchara\LaravelGmail\Traits\Filterable;
 use Amchara\LaravelGmail\Traits\SendsParameters;
+use Amchara\LaravelGmail\Services\MessageCollection;
 use Google_Service_Gmail;
 
 class Message
 {
 
-	use SendsParameters,
-		Filterable;
+    use SendsParameters,
+      Filterable;
 
-	public $service;
+    public $service;
 
-	public $preload = false;
+    public $preload = false;
 
-	public $pageToken;
+    public $pageToken;
 
-	/**
-	 * Optional parameter for getting single and multiple emails
-	 *
-	 * @var array
-	 */
-	protected $params = [];
+    /**
+     * Optional parameter for getting single and multiple emails
+     *
+     * @var array
+     */
+    protected $params = [];
 
-	/**
-	 * Message constructor.
-	 *
-	 * @param  LaravelGmailClass  $client
-	 */
-	public function __construct(LaravelGmailClass $client)
-	{
-		$this->service = new Google_Service_Gmail($client);
-	}
+    /**
+     * Message constructor.
+     *
+     * @param  LaravelGmailClass  $client
+     */
+    public function __construct(LaravelGmailClass $client)
+    {
+        $this->service = new Google_Service_Gmail($client);
+    }
 
-	/**
-	 * Returns next page if available of messages or an empty collection
-	 *
-	 * @return \Illuminate\Support\Collection
-	 */
-	public function next()
-	{
-		if ($this->pageToken) {
-			return $this->all($this->pageToken);
-		} else {
-			return collect([]);
-		}
-	}
+    /**
+     * Returns next page if available of messages or an empty collection
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function next()
+    {
+        if ($this->pageToken) {
+            return $this->all($this->pageToken);
+        } else {
+            return new MessageCollection([], $this);
+        }
+    }
 
-	/**
-	 * Returns a collection of Mail instances
-	 *
-	 * @param  null|string  $pageToken
-	 *
-	 * @return \Illuminate\Support\Collection
-	 */
-	public function all($pageToken = null)
-	{
-		if (!is_null($pageToken)) {
-			$this->add($pageToken, 'pageToken');
-		}
-
-		$messages = [];
-		$response = $this->service->users_messages->listUsersMessages('me', $this->params);
-
-		$this->pageToken = $response->getNextPageToken();
-
-		$allMessages = $response->getMessages();
-
-		foreach ($allMessages as $message) {
-			$messages[] = new Mail($message, $this->preload);
+    /**
+     * Returns a collection of Mail instances
+     *
+     * @param  null|string  $pageToken
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public function all($pageToken = null)
+    {
+        if (!is_null($pageToken)) {
+            $this->add($pageToken, 'pageToken');
         }
 
-		return collect($messages);
-	}
+        $messages = [];
+        $response = $this->getMessagesResponse();
+        $this->pageToken = method_exists( $response, 'getNextPageToken' ) ? $response->getNextPageToken() : null;
 
-	/**
-	 * Returns boolean if the page token variable is null or not
-	 *
-	 * @return bool
-	 */
-	public function hasNextPage()
-	{
-		return !!$this->pageToken;
-	}
+        $allMessages = $response->getMessages();
 
-	/**
-	 * Limit the messages coming from the query
-	 *
-	 * @param  int  $number
-	 *
-	 * @return Message
-	 */
-	public function take($number)
-	{
-		$this->params['maxResults'] = abs((int) $number);
+        foreach ($allMessages as $message) {
+            $messages[] = new Mail($message, $this->preload);
+        }
 
-		return $this;
-	}
+        return new MessageCollection($messages, $this);
+    }
 
-	/**
-	 * @param $id
-	 *
-	 * @return Mail
-	 */
-	public function get($id)
-	{
-		$message = $this->service->users_messages->get('me', $id);
+    private function getMessagesResponse()
+    {
+//		dd($this->params);
+        $responseOrRequest = $this->service->users_messages->listUsersMessages( 'me', $this->params );
 
-		return new Mail($message);
-	}
+        if ( get_class( $responseOrRequest ) === "GuzzleHttp\Psr7\Request" ) {
+            $response = $this->service->getClient()->execute( $responseOrRequest, 'Google_Service_Gmail_ListMessagesResponse' );
 
-	/**
-	 * Preload the information on each Mail objects.
-	 * If is not preload you will have to call the load method from the Mail class
-	 * @return $this
-	 * @see Mail::load()
-	 *
-	 */
-	public function preload()
-	{
-		$this->preload = true;
+            return $response;
+        }
 
-		return $this;
-	}
+        return $responseOrRequest;
+    }
+
+    /**
+     * Returns boolean if the page token variable is null or not
+     *
+     * @return bool
+     */
+    public function hasNextPage()
+    {
+        return !!$this->pageToken;
+    }
+
+    /**
+     * Limit the messages coming from the query
+     *
+     * @param  int  $number
+     *
+     * @return Message
+     */
+    public function take($number)
+    {
+        $this->params['maxResults'] = abs((int) $number);
+
+        return $this;
+    }
+
+    /**
+     * @param $id
+     *
+     * @return Mail
+     */
+    public function get($id)
+    {
+        $message = $this->service->users_messages->get('me', $id);
+
+        return new Mail($message);
+    }
+
+    /**
+     * Preload the information on each Mail objects.
+     * If is not preload you will have to call the load method from the Mail class
+     * @return $this
+     * @see Mail::load()
+     *
+     */
+    public function preload()
+    {
+        $this->preload = true;
+
+        return $this;
+    }
 }
--- src/Traits/SendsParameters.php
+++ src/Traits/SendsParameters.php
@@ -7,18 +7,27 @@ use Illuminate\Support\Arr;
 trait SendsParameters
 {
 
-	/**
-	 * Adds parameters to the parameters property which is used to send additional parameters in the request.
-	 *
-	 * @param $query
-	 * @param  string  $column
-	 */
-	public function add($query, $column = 'q')
-	{
-		if (isset($this->params[$column]) && $column != 'pageToken') {
-			$this->params[$column] = "{$this->params[$column]} $query";
-		} else {
-        $this->params = Arr::add($this->params, $column, $query);
-		}
-	}
+    /**
+     * Adds parameters to the parameters property which is used to send additional parameters in the request.
+     *
+     * @param $query
+     * @param  string  $column
+     */
+    public function add($query, $column = 'q')
+    {
+        if (isset($this->params[$column])) {
+            if ( $column === 'pageToken' ) {
+                $this->params[$column] = $query;
+            } else {
+                $this->params[$column] = "{$this->params[$column]} $query";
+            }
+        } else {
+            $this->params = Arr::add($this->params, $column, $query);
+        }
+    }
+
+    public function addPageToken( $token )
+    {
+        $this->params[ 'pageToken' ] = $token;
+    }
 }
